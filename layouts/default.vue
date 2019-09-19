9b0cf5275b0dc0eb781c2fbe70788e3e7a7f28e4
minho jeon <minhojeon@minhoui-MacBookPro.local>
2019-09-20 00:25:58 +0900

    뷰 첫 커밋
    vue,vuetify,nuxt 설치
    layouts,pages폴더 및 파일들은 기본 테스트 및 뷰티파이 테스트용으로 다음주 삭제예정임. 수정하여서 사용해도 됨

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-col cols="12">
+        <nuxt/>
+    </v-col>
+</template>
+<script>
+import 'vuetify/dist/vuetify.min.css'
+export default {
+
+}
+</script>
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-container>
+        <v-btn>
+            {{name}}
+        </v-btn>
+    </v-container>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            name : '테스트'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
